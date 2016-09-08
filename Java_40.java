b07f31287558dc6b56f1eafc39ecba6df09fec36
yanghengah <yanghengah@163.com>
2016-09-08 17:38:23 +0800

    Update Java_40.java

--- Java_40.java
+++ Java_40.java
@@ -2,7 +2,7 @@ public class Java_40{
 
 
 	public static void main(String[] args){
-		User user = new User("Ñîºã",20,"ÄĞ","13710295382","yanghengah@163.com","¹ãÖİ¿¥ÓòÍøÂç");
+		User user = new User("æ¨**",20,"ç”·","13*******82","y********h@163.com","å¹¿å·*******å…¬å¸");
 		System.out.println(user.ToString());
 	}
 }
